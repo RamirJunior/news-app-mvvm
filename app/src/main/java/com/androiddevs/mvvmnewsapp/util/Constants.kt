f1f441dd859850b4dc5017d3e192e2b0156d0fc5
Ramir Junior <junior.ribeiro0@live.com>
2023-08-10 17:19:05 -0300

    Room settings and Article DAO

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "d97e463e77bd482b886130e77331a8e2"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
